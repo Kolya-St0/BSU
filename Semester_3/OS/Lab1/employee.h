2fb79b2001ac7d9529660a47b42d4363fb9beca7
Kolya <nikolaystanishevskiy@gmail.com>
2025-09-18 20:48:48 +0300

    Added header file for OS first lab

---
+++ Semester_3/OS/Lab1/employee.h
@@ -0,0 +1,18 @@
+﻿// OS_1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+struct employee
+{
+	int num;
+	char name[10];
+	double hours;
+};
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
